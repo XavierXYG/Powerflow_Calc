1efacd100e4e6208e1bfd4cc584361e9828e40c8
Your Name <you@example.com>
2021-01-01 20:26:21 +0800

    admittance

--- Get_Admittance.py
+++ Get_Admittance.py
@@ -1,6 +1,5 @@
 # coding=utf8
 import numpy as np
-import math
 
 
 
@@ -18,19 +17,11 @@ def get_admittance_matrix(Topology):
                 admittance_matrix[i][j] = -Topology[i][j]
     return admittance_matrix
 
-
+'''
 if __name__ == "__main__":
-<<<<<<< HEAD
-
-    a = 1+j
-    b = 1
-    print(a+b)
-
-=======
     test = np.array([[1 + 2j, 2 + 3j, 1 + 1j],
                      [3 + 0j, 4 + 5j, 5],
                      [1 + 0j, 2 + 0j, 3 + 0j]])
     result = get_admittance_matrix(test)
     print(result)
 '''
->>>>>>> 9ba8ed0f98ee18f47ab3a6f4dcac30ccf8ade91c
