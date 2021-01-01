73f06d6238ce05229e57baceaebf3c109a373b60
Capco707 <1298720064@qq.com>
2021-01-01 18:15:10 +0800

    admittance

--- Get_Admittance.py
+++ Get_Admittance.py
@@ -1,13 +1,16 @@
+# coding=utf8
 import numpy as np
+
+
 
 def get_admittance_matrix(Topology):
     num_rows = Topology.shape[0]
     num_columns = Topology.shape[1]
-    admittance_matrix = np.zeros((num_rows, num_columns))
+    admittance_matrix = np.zeros((num_rows, num_columns), dtype=complex)
     for i in range(num_rows):
         for j in range(num_columns):
             if i == j:
-                aux_cal_matrix = np.sum(Topology, axis=0)
+                aux_cal_matrix = np.sum(Topology, axis=1)
                 for num in range(num_rows):
                     admittance_matrix[i][j] = aux_cal_matrix[i]
             else:
@@ -16,9 +19,9 @@ def get_admittance_matrix(Topology):
 
 '''
 if __name__ == "__main__":
-    test = np.array([[1, 2, 1],
-                    [3, 4, 5],
-                    [1, 2, 3]])
+    test = np.array([[1 + 2j, 2 + 3j, 1 + 1j],
+                     [3 + 0j, 4 + 5j, 5],
+                     [1 + 0j, 2 + 0j, 3 + 0j]])
     result = get_admittance_matrix(test)
     print(result)
 '''
