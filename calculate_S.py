81e7da0a83c24bad7348d85c08b25a85f75be8cf
Your Name <you@example.com>
2021-01-01 22:39:57 +0800

    calculate_s

--- calculate_S.py
+++ calculate_S.py
@@ -9,27 +9,27 @@ def power_flow(U_matrix, admittance_matrix):
     y_gnd = numpy.sum(admittance_matrix, axis=1)
     for i in range(0, size_n):
         for j in range(0,size_n):
-            #if (link_vex()):
-            ui = complex(U_matrix[i], U_matrix[i + size_n])
-            uj = complex(U_matrix[j], U_matrix[j + size_n])
-            yi0 = y_gnd[i]
-            yj0 = y_gnd[j]
-            yij = (0 + 0j) - admittance_matrix[i][j]
-            if i==j:
-                S_Topology[i][i] = 0
-                delta_S_Topology[i][i] = ui*ui.conjugate()*yi0.conjugate()                #delta [i][i] calculate the delta S to ground(yi0)
-            else:
-                S_Topology[i][j] = ui*(
-                            ui.conjugate()*yi0.conjugate() + (ui.conjugate()-uj.conjugate())*yij.conjugate())
-                S_Topology[j][i] = uj * (
-                            uj.conjugate() * yj0.conjugate() + (uj.conjugate() - ui.conjugate()) * yij.conjugate())
-                delta_S_Topology[i][j] = S_Topology[i][j] + S_Topology[j][i]
-                delta_S_Topology[j][i] = delta_S_Topology[i][j]
+            if (link_vex()):
+                ui = complex(U_matrix[i], U_matrix[i + size_n])
+                uj = complex(U_matrix[j], U_matrix[j + size_n])
+                yi0 = y_gnd[i]
+                yj0 = y_gnd[j]
+                yij = (0 + 0j) - admittance_matrix[i][j]
+                if i==j:
+                    S_Topology[i][i] = 0
+                    delta_S_Topology[i][i] = ui*ui.conjugate()*yi0.conjugate()                #delta [i][i] calculate the delta S to ground(yi0)
+                else:
+                    S_Topology[i][j] = ui*(
+                                ui.conjugate()*yi0.conjugate() + (ui.conjugate()-uj.conjugate())*yij.conjugate())
+                    S_Topology[j][i] = uj * (
+                                uj.conjugate() * yj0.conjugate() + (uj.conjugate() - ui.conjugate()) * yij.conjugate())
+                    delta_S_Topology[i][j] = S_Topology[i][j] + S_Topology[j][i]
+                    delta_S_Topology[j][i] = delta_S_Topology[i][j]
     return [S_Topology, delta_S_Topology]
 
 
 
-
+'''
 if __name__ =='__main__':
     U = [210, 205, 40, 20]
     y_admittance= numpy.zeros((2,2), dtype=numpy.complex_)
@@ -40,5 +40,5 @@ if __name__ =='__main__':
 
     S_Topology = power_flow(U, y_admittance)
     print(S_Topology)
-
+'''
 
