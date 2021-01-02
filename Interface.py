834cd9f601ab03d9284484ada70e4076358b4c48
Capco707 <1298720064@qq.com>
2021-01-02 20:27:00 +0800

    Interface

--- Interface.py
+++ Interface.py
@@ -7,15 +7,15 @@ def Interface(PQ_bus, PV_bus, VA_bus):  # 输入为三个表达式向量
     num_PV = PV_bus.shape[1]
     num_PQ = PQ_bus.shape[1]
     num_VA = 1 * 2
+    num_total_bus = num_PV + num_PQ + num_VA
     tuple_PV, tuple_PQ, tuple_VA = (), (), ()
-    for i in range(num_PQ):
+    for i in range(num_total_bus):
         tuple_PQ = tuple_PQ + (Global_X[i],)  # 得到初始化的元组
     print(tuple_PQ)
-    for j in range(num_PQ, num_PQ + num_PV):
+    for j in range(num_total_bus):
         tuple_PV = tuple_PV + (Global_X[j],)  # 得到初始化的元组
     print(tuple_PV)
-    start_range, end_range = num_PQ + num_PV, num_PQ + num_PV + num_VA
-    for c in range(start_range, end_range):
+    for c in range(num_total_bus):
         tuple_VA = tuple_VA + (Global_X[c],)  # 得到初始化的元组
     print(tuple_VA)
 
@@ -23,16 +23,16 @@ def Interface(PQ_bus, PV_bus, VA_bus):  # 输入为三个表达式向量
     PQ_trans = lambdify(tuple_PQ, PQ_bus, 'numpy')
     VA_trans = lambdify(tuple_VA, VA_bus, 'numpy')
 
-    initial_value_PV = np.random.rand(num_PV, 1)  # 变量向量赋值的接口
-    initial_value_PQ = np.random.rand(num_PQ, 1)
-    initial_value_VA = np.random.rand(num_VA, 1)
+    initial_value_PV = np.random.rand(num_total_bus, 1)  # 变量向量赋值的接口
+    initial_value_PQ = np.random.rand(num_total_bus, 1)
+    initial_value_VA = np.random.rand(num_total_bus, 1)
 
     PV_input_tuple, PQ_input_tuple, VA_input_tuple = (), (), ()
-    for c1 in range(num_PV):
+    for c1 in range(num_total_bus):
         PV_input_tuple = PV_input_tuple + (initial_value_PV[c1],)  # 得到初始化的元组
-    for c2 in range(num_PQ):
+    for c2 in range(num_total_bus):
         PQ_input_tuple = PQ_input_tuple + (initial_value_PQ[c2],)
-    for c3 in range(num_VA):
+    for c3 in range(num_total_bus):
         VA_input_tuple = VA_input_tuple + (initial_value_VA[c3],)
 
     result_PV = PV_trans(*PV_input_tuple)  # 给变量赋值，将表达式矩阵转化为numpy格式
@@ -49,6 +49,7 @@ def GetBusNum(PQ_bus, PV_bus, VA_bus):
     return [num_PQ, num_PV, num_VA]
 
 
+'''
 if __name__ == "__main__":
     A1 = Matrix([[Global_X[1], Global_X[2], Global_X[3]**2, Global_X[1]]])
     A2 = Matrix([[Global_X[4] + Global_X[5], Global_X[5]]])
@@ -57,3 +58,4 @@ if __name__ == "__main__":
     test = Interface(A1, A2, A3)
     print(test)
     print(GetBusNum(A1, A2, A3))
+'''
