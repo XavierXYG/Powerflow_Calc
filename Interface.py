103191125c4a5a5c30685c8ec5b10d8416c7782b
Capco707 <1298720064@qq.com>
2021-01-07 18:31:19 +0800

    interface

--- Interface.py
+++ Interface.py
@@ -4,50 +4,55 @@ from Global_X import Global_X
 from U_Coefficient import *
 import math
 
-admittance_matrix =  np.array([[1 + 2j, 2 + 3j, 1 + 1j],
-                     [3 + 0j, 4 + 5j, 5],
-                     [1 + 0j, 2 + 0j, 3 + 0j]])
-Global_Y = np.array([[10,20],[10,50000], [242000,0]])
-BusNum = [1, 1, 1]
+admittance_matrix = np.array([[0.12 - 0.16j, -0.12 + 0.16j],
+                              [-0.12 + 0.16j, 0.12 - 0.16j]])
+Global_Y = np.array([[10000000, 20000000], [242000, 0]])
+BusNum = [1, 0, 1]
 
 
 def Interface(input):  # 输入为三个表达式向量
-    PQ_bus = PQ_ef_coefficient(admittance_matrix,  Global_Y, BusNum)
-    PV_bus = PV_ef_coefficient(admittance_matrix,  Global_Y, BusNum)
-    VA_bus = VA_ef_coefficient(Global_Y, BusNum)
+    PQ_bus = PQ_ef_coefficient(admittance_matrix, Global_Y, BusNum)
+    PV_bus = PV_ef_coefficient(admittance_matrix, Global_Y, BusNum)
+    # VA_bus = VA_ef_coefficient(Global_Y, BusNum)
     num_PV = PV_bus.shape[1]
     num_PQ = PQ_bus.shape[1]
-    num_VA = 1 * 2
-    num_total_bus = num_PV + num_PQ + num_VA
-    tuple_PV, tuple_PQ, tuple_VA = (), (), ()
+    # num_VA = 1 * 2
+    num_total_bus = num_PV + num_PQ
+    tuple_PV, tuple_PQ = (), ()
     for i in range(num_total_bus):
         tuple_PQ = tuple_PQ + (Global_X[i],)  # 得到初始化的元组
+    # print(tuple_PQ)
     for j in range(num_total_bus):
         tuple_PV = tuple_PV + (Global_X[j],)  # 得到初始化的元组
-    for c in range(num_total_bus):
-        tuple_VA = tuple_VA + (Global_X[c],)  # 得到初始化的元组
+    # print(tuple_PV)
 
     PV_trans = lambdify(tuple_PV, PV_bus, 'numpy')
     PQ_trans = lambdify(tuple_PQ, PQ_bus, 'numpy')
-    VA_trans = lambdify(tuple_VA, VA_bus, 'numpy')
 
-    initial_value_PV = input[:,np.newaxis]  # 变量向量赋值的接口
-    initial_value_PQ = input[:,np.newaxis]
-    initial_value_VA = input[:,np.newaxis]
+    initial_value_PV = input[:, np.newaxis]  # 变量向量赋值的接口
+    initial_value_PQ = input[:, np.newaxis]
 
-    PV_input_tuple, PQ_input_tuple, VA_input_tuple = (), (), ()
+    PV_input_tuple, PQ_input_tuple = (), ()
     for c1 in range(num_total_bus):
         PV_input_tuple = PV_input_tuple + (initial_value_PV[c1],)  # 得到初始化的元组
     for c2 in range(num_total_bus):
         PQ_input_tuple = PQ_input_tuple + (initial_value_PQ[c2],)
-    for c3 in range(num_total_bus):
-        VA_input_tuple = VA_input_tuple + (initial_value_VA[c3],)
 
     result_PV = PV_trans(*PV_input_tuple)  # 给变量赋值，将表达式矩阵转化为numpy格式
     result_PQ = PQ_trans(*PQ_input_tuple)
-    result_VA = VA_trans(*VA_input_tuple)
-    result = np.r_[result_PQ[0], result_PV[0], result_VA[0]]
+    if ~np.any(result_PV):
+        result = np.r_[result_PQ[0]]
+    elif ~np.any(result_PQ):
+        result = np.r_[result_PV[0]]
+    else:
+        result = np.r_[result_PQ[0], result_PV[0]]
     return result
+
+
+def GetBusNum(PQ_bus, PV_bus):
+    num_PV = PV_bus.shape[1]
+    num_PQ = PQ_bus.shape[1]
+    return [num_PQ, num_PV]
 
 
 '''
