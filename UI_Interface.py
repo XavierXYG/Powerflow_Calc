9c6b0e9eeb0b5fc8a8aeb73ca5a3e82d43f2815d
Your Name <you@example.com>
2021-01-12 10:16:13 +0800

    linkedge

--- UI_Interface.py
+++ UI_Interface.py
@@ -15,3 +15,14 @@ def link_vex(node1, node2, scene):
     return False
 
 
+def link_edge(node1, node2, scene):
+    for gr_edge in scene.edges:
+        start_node = gr_edge.edge_wrap.start_item
+        end_node = gr_edge.edge_wrap.end_item
+        if node1 == start_node:
+            if node2 == end_node:
+                return gr_edge
+        elif node2 == start_node:
+            if node1 == end_node:
+                return gr_edge
+    return None
